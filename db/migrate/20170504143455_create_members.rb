0b03d04cac64b920f2dfef97ea8253e0d1577be9
田中健 <takeru@tanakaken-no-MacBook-Pro.local>
2017-05-04 23:38:12 +0900

    edit migration of member

--- db/migrate/20170504143455_create_members.rb
+++ db/migrate/20170504143455_create_members.rb
@@ -1,6 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
+      t.references :user, foreign_key: true
+      t.references :group, foreign_key: true
 
       t.timestamps
     end

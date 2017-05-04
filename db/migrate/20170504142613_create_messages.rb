1648c80ccec3d6adcc8ed4a395d0ba8de2016370
田中健 <takeru@tanakaken-no-MacBook-Pro.local>
2017-05-04 23:34:33 +0900

    edit migration of message

--- db/migrate/20170504142613_create_messages.rb
+++ db/migrate/20170504142613_create_messages.rb
@@ -1,6 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
+      t.string :image
+      t.integer :group_id
+      t.references :group, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end

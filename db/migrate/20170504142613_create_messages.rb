3c37963bf63c6010de63058fbee89ae6061b12ca
田中健 <takeru@tanakaken-no-MacBook-Pro.local>
2017-05-05 19:11:13 +0900

    correct migration of message
    t.integer :group_id   →    t.text :text

--- db/migrate/20170504142613_create_messages.rb
+++ db/migrate/20170504142613_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.string :image
-      t.integer :group_id
+      t.text :text
       t.references :group, foreign_key: true, null: false
       t.references :user, foreign_key: true, null: false
 

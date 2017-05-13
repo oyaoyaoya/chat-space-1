d26c55f03927e38752e83f933ea52f7ca31c0d74
田中健 <takeru.tt5672@gmqil.com>
2017-05-14 00:46:37 +0900

    show flash-message after updating
    if you can update your account, then you can see flash
    message,”アカウントの更新が完了しました”, on your index-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-
   def update
+    user = User.find(current_user.id)
+    user.update(user_params)
+    redirect_to :root, :notice => "アカウントの更新が完了しました。"
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end

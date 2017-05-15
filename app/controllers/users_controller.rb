62e3b89ed9d26a37cc3f044a2476a113cfcdc129
田中健 <takeru.tt5672@gmqil.com>
2017-05-15 14:39:20 +0900

    show flash message after failing to update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   def update
     user = User.find(current_user.id)
     user.update(user_params)
-    redirect_to :root, :notice => "アカウントの更新が完了しました。"
+
+    return redirect_to :edit_user_registration, :alert => "更新に失敗しました。入力しなおしてください" unless user.valid?
+
+    redirect_to :root , :notice => "アカウントの更新が完了しました。"
   end
 
 private

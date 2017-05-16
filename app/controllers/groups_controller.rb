9464707a571b8e82726e13b1bd12b3aa19d92e8d
田中健 <takeru.tt5672@gmqil.com>
2017-05-16 20:57:38 +0900

    edit groups_controller
    edit groups_controller to save the group and these members

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,20 @@ class GroupsController < ApplicationController
   end
 
   def create
+    group = Group.create(group_params)
+    member_params[:user_id].each do |id|
+      Member.create(user_id: id, group_id: group.id)
+    end
+    redirect_to :root
+  end
+
+private
+  def member_params
+    params.require(:member).permit(:user_id => [])
+  end
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 
 end

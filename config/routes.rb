f29fb8c8faae6b3c5fd9484e34d4eba27e65321a
田中健 <takeru.tt5672@gmqil.com>
2017-05-17 17:38:54 +0900

    set up routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'messages#index'
   resources :messages, only: :create
   resources :users, only: :update
-  resources :groups, only: [:new, :edit, :create, :update]
+  resources :groups, except: [:index, :destroy]
 end

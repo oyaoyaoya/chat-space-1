4297cc63fa91b573ba10c87b8d9b3569934f30fa
田中健 <takeru.tt5672@gmqil.com>
2017-05-21 09:39:16 +0900

    edit  routings
    to create nested routing of message#create
    ->/groups/:group_id/messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
-  resources :messages, only: :create
   resources :users, only: :update
-  resources :groups, except: %i(index destroy)
+  resources :groups, except: %i(index destroy) do
+    resources :messages, only: :create
+  end
 end

e45de8520deb8345eea536b440c4198d2b0700d8
田中健 <takeru.tt5672@gmqil.com>
2017-05-21 09:52:21 +0900

    edit messages#create to save message
    if params.require(:message).permit(:text) => null
    then
    show flash-error-message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,18 @@ class MessagesController < ApplicationController
   end
 
   def create
+    message = Message.create(message_params)
+
+    unless message.valid?
+      redirect_to :back, :alert => "#{message.errors.full_messages[0]}"
+    else
+      redirect_to :back
+    end
+
+  end
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 
 end

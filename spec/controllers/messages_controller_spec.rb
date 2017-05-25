fec5d1dd6795800d969cb55e4896be43ceead743
田中健 <takeru.tt5672@gmqil.com>
2017-05-25 14:28:31 +0900

    edit not to use instance variable
    @groups -> let(:group)

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 describe MessagesController, type: :controller do
   let(:user) { create(:user) }
+  let(:group) {user.groups.first}
+
 
   describe 'GET #index' do
     context "ユーザーがログインしている場合" do
       before do
         login_user user
-        @group    = user.groups.first
-        @groups   = user.groups
-        @messages = @group.messages
-        get :index, params: { group_id: @group }
+        get :index, params: { group_id: group }
       end
 
       it "@groupsにユーザーが属しているグプープを割り当てること" do
-       expect(assigns(:groups)).to match(@groups)
+        groups = user.groups
+        expect(assigns(:groups)).to match(groups)
       end
 
       it "@groupに表示したいグループを割り当てること" do
-        expect(assigns(:group)).to eq @group
+        expect(assigns(:group)).to eq group
       end
 
       it "@messageに新規メッセージが割り当てられていること" do
@@ -26,7 +26,8 @@ describe MessagesController, type: :controller do
       end
 
       it "@messagesにグループ内でのメッセージを割り当てること" do
-        expect(assigns(:messages)).to match(@messages.sort{ |a, b| a.created_at <=> b.created_at })
+        messages = group.messages
+        expect(assigns(:messages)).to match(messages.sort{ |a, b| a.created_at <=> b.created_at })
       end
 
       it "indexテンプレートを表示すること" do
@@ -36,11 +37,11 @@ describe MessagesController, type: :controller do
     end
     context "ユーザーがログインしていない場合" do
       it "ログインページにリダイレクトされること" do
-        group = user.groups.first
         get :index, params: {group_id: group}
         expect(response).to redirect_to new_user_session_path
       end
     end
   end
+
 
 end

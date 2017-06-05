835270903604160a5d17cd10bac09667538ba05d
田中健 <takeru.tt5672@gmqil.com>
2017-06-05 19:16:33 +0900

    add automatic update function
    update every 5 seconds by using function ‘setInterval’
    →
    asynchronous communication　by using Ajax
    →
    insert message　which are not already displayed

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,15 @@
 $(function() {
+
   // viewに差し込むhtmlを作成する関数
   function buildChatbox(data) {
     var html =   `
-      <div class="chatbox">
+      <div class="chatbox", data_messageid= ${data.id}>
         <div class="chatbox__data">
           <span class="chatbox__data__name">
             ${data.name}
           </span>
           <span class="chatbox__data__date">
-            ${data.day}
+            ${data.created_at}
           </span>
         <div class="chatbox__gap">
         </div>
@@ -19,32 +20,70 @@ $(function() {
         </div>
       </div>
     `;
-  return html;
+  $('.chatArea').append(html);
   };
-  // Sendで発火し、入力したメッセージを取得して非同期通信を行う
-  $('#new_message').on('submit', function(e) {
-    e.preventDefault();
-    var inputChat = $('.inputbox__message').val();
 
+  // 非同期通信でメッセージを投稿する関数
+  function postMessage(text) {
     $.ajax({
       type: 'POST',
        url:  $('#new_message').attr('action'),
   dataType: 'json',
       data: {
       message: {
-         text: inputChat,
+         text: text,
         }
-    }})
-      // チャットメッセージを差し込む
+      }
+    })
       .done(function(data) {
-        $('.chatArea').append(buildChatbox(data));
+        buildChatbox(data);
         $('.inputbox__message').val('');
       })
-      // エラーメッセージの表示
       .fail(function(){
         $('body').prepend('<p class="flashbox--color-red">テキストを入力してください</p>');
       });
-    });
+  }
+
+  // 非同期通信でメッセージを更新する関数
+  function renewMessage() {
+    $.ajax({
+      type: 'GET',
+       url:  $('#new_message').attr('action')
+     })
+      .done(function(data) {
+        if (data.messages.length) {
+          // まだ表示されていないメッセージのみを取得する処理
+          newMessage = data.messages.filter(function(message) {
+            if ( message.id > $('.chatbox:last').attr('data_messageid') ) {
+              return true;
+            } else if ($('.chatbox:last').length == 0) {
+              return true;
+            } else {
+              return false;
+            }
+          });
+          $.each(newMessage,function(i,message) {
+              buildChatbox(message);
+          });
+        }
+    })
+      .fail(function(){
+        $('body').prepend('<p class="flashbox--color-red">更新に失敗しました</p>');
+      });
+  }
+
+
+  // Sendで発火し、入力したメッセージを取得後postMessageを実行
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var inputChat = $('.inputbox__message').val();
+    postMessage(inputChat);
+  });
+
+  // チャットページが表示されている際、5秒ごとにrenewMessageを実行
+  if ( $('.inputArea').length ) {
+    setInterval(renewMessage(),5000);
+  }
 
 });
 
--- app/views/messages/create.jbuilder
+++ app/views/messages/create.jbuilder
@@ -2,3 +2,4 @@ json.text @message.text
 json.name @message.user.name
 json.day  @message.created_at.strftime("%Y-%m-%d %H:%M:%S")
 json.id   @message.id
+
